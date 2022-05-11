392760c09a552dc4a5e9054ceb5e6d7d33efaa4d
Denis Oleynik <denis1riddle@gmail.com>
2022-05-11 12:51:54 +0300

    database added

--- app.py
+++ app.py
@@ -2,23 +2,44 @@ from PyQt6.QtWidgets import QApplication
 
 from ui import WindowMain, WindowAuth
 import sys
+import sqlite3
+
+
+class Database:
+    def __init__(self):
+        self.connection = sqlite3.connect("mailposts.db")
+        self.cursor = self.connection.cursor()
+
+    def execute_read_query(self, query: str) -> list:
+        try:
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+
+            print(f"Request \"{query}\" executed successfully!")
+            return data
+
+        except sqlite3.Error as error:
+            print(error)
+        finally:
+            if self.connection:
+                self.connection.close()
+
+
 
 
 class Application:
     def __init__(self):
         self.app = QApplication(sys.argv)
+        self.database = Database()
         self.window = WindowMain()
-        self.window2 = WindowAuth()
 
         self.window.show()
-        self.window2.show()
 
         sys.exit(self.app.exec())
 
 
 def main():
     app = Application()
-
 
 if __name__ == '__main__':
     main()
