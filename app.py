a511ebf534b912304950a2fb8b6f2ca633a94af2
Denis Oleynik <denis1riddle@gmail.com>
2022-05-17 15:09:05 +0300

    Database features done

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 import sys
 
+import database
+from database import Database
+
 from PyQt5 import QtWidgets
 import UI
 
+from loguru import logger
 
 
-class AuthWindow(UI.Ui_AuthWindow):
+class AuthWindow(QtWidgets.QMainWindow, UI.Ui_AuthWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.validate_auth_data)
 
-    def user_auth(self):
-        pass
+        self.database = Database()
+
+
+    def validate_auth_data(self):
+        login = self.Input_Login.text()
+        password = self.Input_Pass.text()
+
+        query = f'SELECT * FROM users WHERE login="{login}" AND password="{password}";'
+        data = self.database.execute_read_query(query)
+        if data:
+            logger.warning(data)
+
+
+
 
 
 class MainWindow(QtWidgets.QMainWindow, UI.Ui_MainWindow):
@@ -24,18 +41,27 @@ class MainWindow(QtWidgets.QMainWindow, UI.Ui_MainWindow):
         self.MenuBar_Exit.triggered.connect(self.menubar_exit)
 
     def menubar_auth(self):
-        print('login')
+        logger.info("'Login' button triggered!")
+        self.AuthWindow = AuthWindow()
+        self.AuthWindow.show()
 
     def menubar_exit(self):
-        print('exit')
+        logger.info("'Exit' button triggered!")
+        sys.exit()
 
 
 
 if __name__ == '__main__':
+    # start_time = time.time()
+
     app = QtWidgets.QApplication([])
     StartWindow = MainWindow()
     StartWindow.show()
 
+    # logger.info(f'Program finished in {int(start_time-time.time())} s.')
+
     sys.exit(app.exec())
+
+
 
 
--- database.py
+++ database.py
@@ -1,23 +1,42 @@
 import sqlite3
+from loguru import logger
+
 
 class Database:
     def __init__(self):
         self.connection = sqlite3.connect("mailposts.db")
         self.cursor = self.connection.cursor()
 
+        try:
+            pass
+            # Create users table:
+            # table_users = """
+            # CREATE TABLE IF NOT EXISTS users (
+            # login VARCHAR(255) NOT NULL PRIMARY KEY,
+            # password VARCHAR(255) NOT NULL);
+            # """
+            # self.connection.execute(table_users)
+            # self.connection.commit()
+
+        except sqlite3.Error as err:
+            logger.exception(err)
+
     def execute_read_query(self, query: str) -> list:
         try:
             self.cursor.execute(query)
             data = self.cursor.fetchall()
-
-            print(f"Request \"{query}\" executed successfully!")
+            logger.debug(f"Request \"{query}\" executed successfully!")
             return data
 
         except sqlite3.Error as error:
-            print(error)
-        finally:
-            if self.connection:
-                self.connection.close()
+            logger.exception(error)
+
 
     def execute_write_query(self, query: str):
-        pass
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+            logger.debug(f"Request \"{query}\" executed successfully!")
+        except sqlite3.Error as err:
+            logger.exception(err)
+
